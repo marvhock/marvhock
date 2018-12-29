7ad4ac1c2ec52433afadeceab3555698f0b55fe8
mhockenjos <marvin.hockenjos@gmail.com>
2018-12-29 17:12:51 +0100

    Remove fastclick and refactor css

--- src/css/postcss.config.js
+++ src/css/postcss.config.js
@@ -5,6 +5,9 @@ module.exports = {
 	   browsers: ['last 2 versions', '> 5%'],
 	  },
     'postcss-nested': {},
-    'postcss-simple-vars': {}
+    'postcss-simple-vars': {},
+    'cssnano': {
+      preset: 'default',
+    }
 	}
 };
--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,1 @@
 import './../css/main.scss';
-import FastClick from './fastclick.js';
-FastClick.attach(document.body);
